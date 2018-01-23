9824b12a84052c3a992b5b237899deaf168b5723
Vitali Stukov <vstukov@beltechsoftware.com>
2018-01-23 17:24:59 +0300

    Stat

---
+++ lib/stat_exmo.rb
@@ -0,0 +1,35 @@
+class StatExmo
+
+
+  def start
+
+    currencies = Currency.parse_to_currencies(Exmo.currency)
+    pairs = Pair.parse_to_pairs(Exmo.pair_settings.keys, currencies)
+    start_currency = Currency.find_selected_cur(currencies, 'USD')
+
+
+    100.times do |i|
+      puts i
+      orders = Exmo.order_book(pairs, limit: Exmo.limit_order_book)
+      Pair.add_orders(pairs, orders)
+      @graph ||= Graph.new(currencies, pairs)
+      @ways ||= @graph.ways(start_currency)
+
+      forkFinder = ForkFinder.new(commission: Exmo.commission, pairs: pairs, profit_percent: 0, limit: 10)
+
+      forks = forkFinder.find(@ways, coins: 10)
+
+      forks.each do |fork|
+        File.open("#{Rails.root}/log/#{fork.to_s}", 'a+') do |file|
+          file.write("#{Time.now.to_s} : #{fork.percentage_profit.round(3)}")
+          file.write("\n")
+        end
+      end
+    end
+
+
+
+    nil
+  end
+
+end
