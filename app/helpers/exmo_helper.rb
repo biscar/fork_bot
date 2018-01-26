e9da7dd9074b96172be7a124cf2236f33a3eafb9
Vitali Stukov <vstukov@beltechsoftware.com>
2018-01-26 17:51:56 +0300

    Fix bug.

--- app/helpers/exmo_helper.rb
+++
@@ -1,9 +0,0 @@
-module ExmoHelper
-  def show_details_url
-    '/exmo/show_details'
-  end
-
-  def refresh_fork_url
-    '/exmo/refresh_fork'
-  end
-end
--- app/helpers/poloniex_helper.rb
+++
@@ -1,9 +0,0 @@
-module PoloniexHelper
-  def show_details_url
-    '/poloniex/show_details'
-  end
-
-  def refresh_fork_url
-    '/poloniex/refresh_fork'
-  end
-end
